f5bd2fea4c7c3ee747a31a9d2a98ac9426af0e5f
George <george.coker@hotmail.com>
2019-08-14 21:37:55 +0100

    Implemented GET and View for Edit page.

--- SpiceRestaurant/Areas/Admin/Controllers/CategoryController.cs
+++ SpiceRestaurant/Areas/Admin/Controllers/CategoryController.cs
@@ -45,5 +45,20 @@ namespace SpiceRestaurant.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
     }
 }
