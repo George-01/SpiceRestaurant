eb29cebb9815d4e94009f93b8184b6afb0fd49f0
George <george.coker@hotmail.com>
2019-08-17 23:34:51 +0100

    Created Details page and implemented post back to Edit and Index pages

--- SpiceRestaurant/Areas/Admin/Controllers/CategoryController.cs
+++ SpiceRestaurant/Areas/Admin/Controllers/CategoryController.cs
@@ -105,5 +105,32 @@ namespace SpiceRestaurant.Areas.Admin.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        //GET - Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+           var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //POST - DETAILS
+        [HttpPost, ActionName("Details")]
+        public async Task<IActionResult> DetailsPost(int id)
+        {
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return RedirectToAction(nameof(Edit));
+        }
     }
 }
