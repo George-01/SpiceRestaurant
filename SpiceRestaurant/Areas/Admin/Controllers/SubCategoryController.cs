990bbb16e0dd41a563b79f5b458e31f01c9218d9
George <george.coker@hotmail.com>
2019-08-19 02:59:03 +0100

    Created Extension Methods for Cascaded Dropdown form controls

--- SpiceRestaurant/Areas/Admin/Controllers/SubCategoryController.cs
+++ SpiceRestaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpiceRestaurant.Data;
+using SpiceRestaurant.Models.ViewModels;
 
 namespace SpiceRestaurant.Areas.Admin.Controllers
 {
@@ -21,6 +22,24 @@ namespace SpiceRestaurant.Areas.Admin.Controllers
         {
             var subCategory = await _db.SubCategory.Include(s => s.Category).ToListAsync();
             return View(subCategory);
+        }
+
+        //GET -CREATE
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory
+                                            .OrderBy(p => p.Name)
+                                            .Select(p => p.Name)
+                                            .Distinct()
+                                            .ToListAsync()
+            };
+
+            return View(model);
+
         }
     }
 }
