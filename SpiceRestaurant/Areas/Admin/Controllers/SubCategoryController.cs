25b2016a39ce6a510b37fda8c086c7bdda8976ad
George <george.coker@hotmail.com>
2019-08-19 02:13:09 +0100

    Created Sub-Category Controller and Index view.

---
+++ SpiceRestaurant/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpiceRestaurant.Data;
+
+namespace SpiceRestaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var subCategory = await _db.SubCategory.Include(s => s.Category).ToListAsync();
+            return View(subCategory);
+        }
+    }
+}
