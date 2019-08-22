f16dbc74a9e59eb97bc5a3811f7a2bf7d118676e
George <george.coker@hotmail.com>
2019-08-22 04:01:11 +0100

    Added Create View for Menu Item

--- SpiceRestaurant/Areas/Admin/Controllers/MenuItemController.cs
+++ SpiceRestaurant/Areas/Admin/Controllers/MenuItemController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpiceRestaurant.Data;
+using SpiceRestaurant.Models.ViewModels;
 
 namespace SpiceRestaurant.Areas.Admin.Controllers
 {
@@ -15,15 +16,29 @@ namespace SpiceRestaurant.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IHostingEnvironment _hostingEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
         {
             this._db = db; 
             this._hostingEnvironment = hostingEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
