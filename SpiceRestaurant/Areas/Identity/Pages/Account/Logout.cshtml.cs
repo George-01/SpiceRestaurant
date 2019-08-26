3abcfaad777df91911ec462260e12fa97856dc08
George <george.coker@hotmail.com>
2019-08-26 16:37:45 +0100

    Implemented User management functionality

--- SpiceRestaurant/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ SpiceRestaurant/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -36,7 +36,7 @@ namespace SpiceRestaurant.Areas.Identity.Pages.Account
             }
             else
             {
-                return Page();
+                return RedirectToPage("Logout");
             }
         }
     }
