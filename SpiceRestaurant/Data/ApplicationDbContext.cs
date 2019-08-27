a0e52b677d8074846c01cd4ce143e6bffbdb5001
George <george.coker@hotmail.com>
2019-08-27 22:47:51 +0100

    Added OrderHeader and Order Details Models and Updated Db

--- SpiceRestaurant/Data/ApplicationDbContext.cs
+++ SpiceRestaurant/Data/ApplicationDbContext.cs
@@ -22,5 +22,9 @@ namespace SpiceRestaurant.Data
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
 
         public DbSet<ShoppingCart> ShoppingCart { get; set; }
+
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
